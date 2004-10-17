c10ae0177601f76d44783b0c1a4a438e855b54d9
Michael Schilli <m@perlmeister.com>
2004-10-17 07:20:34 +0000

    Import of MSCHILLI/Sysadm-Install-0.05 from CPAN.
    gitpan-cpan-distribution: Sysadm-Install
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MSCHILLI/Sysadm-Install-0.05.tar.gz
    gitpan-cpan-author:       MSCHILLI
    gitpan-cpan-maturity:     released

--- lib/Sysadm/Install.pm
+++ lib/Sysadm/Install.pm
@@ -6,6 +6,8 @@ use 5.006;
 use strict;
 use warnings;
 
+our $VERSION = '0.05';
+
 use File::Copy;
 use File::Path;
 use Log::Log4perl qw(:easy);
@@ -13,14 +15,40 @@ use LWP::Simple;
 use File::Basename;
 use Archive::Tar;
 use Cwd;
+use File::Temp;
 
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT = qw(cp rmf mkd cd make cdback download untar pie slurp blurt);
+our @EXPORTABLE = qw(
+cp rmf mkd cd make 
+cdback download untar 
+pie slurp blurt mv tap
+);
 
-our $VERSION = '0.04';
+our %EXPORTABLE = map { $_ => 1 } @EXPORTABLE;
 
 our @DIR_STACK;
+
+##################################################
+sub import {
+##################################################
+    my($class) = shift;
+
+    no strict qw(refs);
+
+    my $caller_pkg = caller();
+
+    my(%tags) = map { $_ => 1 } @_;
+
+        # Export all
+    if(exists $tags{':all'}) {
+        %tags = map { $_ => 1 } @EXPORTABLE;
+    }
+
+    for my $func (keys %tags) {
+        die __PACKAGE__ . 
+            "doesn't export \"$func\"" unless exists $EXPORTABLE{$func};
+        *{"$caller_pkg\::$func"} = *{$func};
+    }
+}
 
 =pod
 
@@ -30,7 +58,7 @@ Sysadm::Install - Typical installation tasks for system administrators
 
 =head1 SYNOPSIS
 
-  use Sysadm::Install;
+  use Sysadm::Install qw(:all);
 
   my $INST_DIR = '/home/me/install/';
 
@@ -49,6 +77,9 @@ Sysadm::Install - Typical installation tasks for system administrators
   pie(sub { s/Today/scalar localtime()/ge; $_; }, "build.dat");
 
   make("test install");
+
+     # run a cmd and tap into stdout and stderr
+  my($stdout, $stderr) = tap("ls -R");
 
 =head1 DESCRIPTION
 
@@ -80,6 +111,21 @@ sub cp {
 ###############################################
     INFO "cp $_[0] $_[1]";
     File::Copy::copy @_ or LOGDIE "Cannot copy $_[0] to $_[1] ($!)";
+}
+
+=pod
+
+=item C<mv($source, $target)>
+
+Move a file from C<$source> to C<$target>. C<target> can be a directory.
+
+=cut
+
+###############################################
+sub mv {
+###############################################
+    INFO "mv $_[0] $_[1]";
+    File::Copy::move @_ or LOGDIE "Cannot move $_[0] to $_[1] ($!)";
 }
 
 =pod
@@ -362,6 +408,37 @@ sub blurt {
         or LOGDIE "Cannot open $file for writing ($!)";
     print FILE $data;
     close FILE;
+}
+
+=pod
+
+=item C<($stdout, $stderr) = tap($cmd)>
+
+Rund a command C<$cmd> in the shell, capture STDOUT and STDERR, and
+return them as strings.
+
+=cut
+
+###############################################
+sub tap {
+###############################################
+    my($cmd) = @_;
+
+    my $tmpfh   = File::Temp->new(UNLINK => 1, SUFFIX => '.dat');
+    my $tmpfile = $tmpfh->filename();
+
+    DEBUG "tempfile $tmpfile created";
+
+    $cmd = "$cmd 2>$tmpfile |";
+    INFO "tap $cmd";
+
+    open PIPE, $cmd or LOGDIE "open $cmd | failed ($!)";
+    my $stdout = join '', <PIPE>;
+    close PIPE or LOGDIE "close $cmd failed ($!)";
+
+    my $stderr = slurp($tmpfile);
+
+    return ($stdout, $stderr);
 }
 
 =pod
